b16fd5c9e30a127e2d6cfa27745def2a4d7942ee
634200 <3366438434@qq.com>
2024-02-27 15:19:14 +0800

    开发了一个hellow world接口 访问地址为http：//127.0.0.1：8880/hellow

--- src/main/java/com/java/wiki/config/WikiApplication.java
+++ src/main/java/com/java/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.java.wiki;
+package com.java.wiki.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
-
+@ComponentScan("com.java")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
