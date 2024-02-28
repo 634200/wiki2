34cef1d345627e29ae15583f203182354dfa14af
634200 <3366438434@qq.com>
2024-02-28 22:56:21 +0800

    添加一个http检测脚本的问题解决成功

--- src/main/java/com/java/wiki/WikiApplication.java
+++ src/main/java/com/java/wiki/WikiApplication.java
@@ -1,11 +1,11 @@
-package com.java.wiki.config;
+package com.java.wiki;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-@ComponentScan("com.java")
+//@ComponentScan("com.java")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
@@ -14,6 +14,9 @@ public class WikiApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！");
         LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+
+//        ConfigurableApplicationContext run = SpringApplication.run(WikiApplication.class, args);
     }
 }
 
--- src/main/java/com/java/wiki/controller/Testcontroller.java
+++ src/main/java/com/java/wiki/controller/Testcontroller.java
@@ -2,14 +2,23 @@ package com.java.wiki.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
 public class Testcontroller {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello world!";
+        return "Hello World!";
     }
 
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello world!Post,"+name;
+    }
 }
