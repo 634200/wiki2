53590add228ec573f4c46c1a24a88b82efb836e8
634200 <3366438434@qq.com>
2024-02-29 11:15:58 +0800

    更新了部分自定义配置

--- src/main/java/com/java/wiki/controller/Testcontroller.java
+++ src/main/java/com/java/wiki/controller/Testcontroller.java
@@ -1,6 +1,7 @@
 package com.java.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +12,12 @@ import java.util.Map;
 
 @RestController
 public class Testcontroller {
+    @Value("$test hello=Hello")
+    private String testHello;
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
-
-
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "Hello world!Post,"+name;
