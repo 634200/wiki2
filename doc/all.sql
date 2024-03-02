11021efb3f684a57a7ffe7cfd4a60a8c7b81b6c3
634200 <3366438434@qq.com>
2024-03-02 22:35:40 +0800

    数据库进行了更换，换成了wikidev

--- doc/all.sql
+++ doc/all.sql
@@ -1,7 +1,8 @@
-drop table if exists 'tests';
-create table 'tests'(
-    'id' bigint not null comment 'id',
-    'name' varchar(50) comment '名称',
-    'password' varchar(50) comment '密码',
-    primary key ('id')
+create table `tests`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
 )engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `tests` (id,name,password) values (1,'测试','password');
