b7c918108ca3eb1397d134db43f75af6a9bdcfd4
634200 <3366438434@qq.com>
2024-03-19 15:56:43 +0800

    为保存接口增加参数校验

--- src/main/java/com/java/wiki/controller/EbookController.java
+++ src/main/java/com/java/wiki/controller/EbookController.java
@@ -27,7 +27,7 @@ public class EbookController {
     }
 
     @PostMapping("/save")
-    public CommonResp save(@RequestBody EbookNameSaveReq req) {
+    public CommonResp save(@Valid @RequestBody EbookNameSaveReq req) {
         CommonResp resp = new CommonResp<>();
         ebookService.save(req);
         return resp;
